a888d2a25adb86a1e4522d56e85159c55e943eaf
GrayXcode <hoyx0101@gmail.com>
2022-08-23 09:50:10 +0000

    working

--- lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
+++ lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
@@ -7,9 +7,9 @@ from urllib.request import urlopen
 
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = 'asahel.eu.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+API_AUDIENCE = 'image'
 
 
 class AuthError(Exception):
@@ -100,25 +100,52 @@ def verify_decode_jwt(token):
                 'description': 'Unable to parse authentication token.'
             }, 400)
     raise AuthError({
-                'code': 'invalid_header',
+        'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
-            }, 400)
+    }, 400)
 
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+def check_auth_permission(permission, payload):
+    # payload['permissions'] houses the permissions assigned to roles
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid claim',
+            'description': 'Permissions not set in JWT'
+        }, 400)
 
-    return wrapper
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'Permission Not Found',
+            'description': 'Invalid permission'
+        }, 403)
 
-@app.route('/headers')
-@requires_auth
-def headers(payload):
-    print(payload)
-    return 'Access Granted'
+    return True
+
+
+def require_auth(permission=''):
+    def require_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+
+            check_auth_permission(permission, payload)
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return require_auth_decorator
+
+
+# @app.route('/headers')
+# @requires_auth
+# def headers(payload):
+#     print(payload)
+#     return 'Access Granted'
+
+@app.route('/images')
+@require_auth('get:images')
+def get_image(payload):
+    return 'Successful'
